6962a07fb691bcd2f9622be358f15acb177e3dd4
camilossantos2809 <camilossantos.2809@gmail.com>
2025-03-09 14:42:11 -0300

    initial setup

---
+++ api/src/main/kotlin/Routing.kt
@@ -0,0 +1,16 @@
+package io
+
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import java.sql.Connection
+import java.sql.DriverManager
+import org.jetbrains.exposed.sql.*
+
+fun Application.configureRouting() {
+}
