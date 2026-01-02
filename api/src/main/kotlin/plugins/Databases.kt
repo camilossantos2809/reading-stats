3eeec1b3a2aee7ba475ab09109e44c6b870915db
camilossantos2809 <camilossantos.2809@gmail.com>
2026-01-02 15:59:51 -0300

    rollback to postgres

--- api/src/main/kotlin/plugins/Databases.kt
+++ api/src/main/kotlin/plugins/Databases.kt
@@ -1,27 +1,19 @@
 package io.plugins
 
-import io.development
 import io.ktor.server.application.*
 import org.jetbrains.exposed.sql.Database
-import java.io.File
-import java.util.*
 
-val localProperties = Properties().apply {
-    load(File("local.properties").reader())
-}
 
 fun Application.configureDatabases() {
 //    Postgres/docker
-//    val dbHost = System.getenv("DB_HOST") ?: "localhost"
-//    val dbPort = System.getenv("DB_PORT") ?: "5432"
-//    val dbName = System.getenv("DB_NAME") ?: "myprojectdb"
-//    val dbUser = System.getenv("DB_USER") ?: "admin"
-//    val dbPassword = System.getenv("DB_PASSWORD") ?: "secret"
-//    val dbUrl = "jdbc:postgresql://$dbHost:$dbPort/$dbName"
-//
-//    Database.connect(
-//        dbUrl, user = dbUser, password = dbPassword
-//    )
-    val sqlitePath: String = if (development)  localProperties.getProperty("sqlitePath")?:"" else "data/readingstats.db"
-    Database.connect("jdbc:sqlite:$sqlitePath", "org.sqlite.JDBC")
+    val dbHost = System.getenv("DB_HOST") ?: "localhost"
+    val dbPort = System.getenv("DB_PORT") ?: "5432"
+    val dbName = System.getenv("DB_NAME") ?: "reading_stats"
+    val dbUser = System.getenv("DB_USER") ?: "admin"
+    val dbPassword = System.getenv("DB_PASSWORD") ?: "secret"
+    val dbUrl = "jdbc:postgresql://$dbHost:$dbPort/$dbName"
+
+    Database.connect(
+        dbUrl, user = dbUser, password = dbPassword
+    )
 }
